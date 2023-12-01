90bad3a33413e2c5e20fb239c7792b33107958fa
FailedFeather37 <126341708+FailedFeather37@users.noreply.github.com>
2023-12-01 21:26:33 +0100

    Add files via upload

--- fourmi_langton.py
+++ fourmi_langton.py
@@ -1,4 +1,4 @@
-from tkinter import *
+﻿from tkinter import *
 
 HAUT = 0
 GAUCHE= 1
@@ -8,8 +8,11 @@ DROITE = 3
 VIDE = 0
 OCCUPE = 1
 
-TAILLE_GRILLE = 100 # nombre de colonne et ligne
+TAILLE_GRILLE = 100
 
+
+#x_grille y_grille 100*100 grille pour la fourmi
+#x y 500*500 valeur de la fourmi
 # definition des fonctions de dessin pour les fourmis
 class CanvasFourmi(Canvas):
     def __init__(self, fenetre):
@@ -19,62 +22,66 @@ class CanvasFourmi(Canvas):
 
     def dessine_case(self, x_grille, y_grille, couleur):
         taille = self.taille_case
-        x, y = x_grille, y_grille
+        x, y = x_grille*taille, y_grille*taille
         x_prime=x+taille
         y_prime=y+taille
         self.create_rectangle(x,y,x_prime,y_prime,fill=couleur,outline=couleur)
 
-# definition d'une fourmi de langton
+
 class Fourmi:
-    # constructeur avec le canvas pour le dessin, la position initiale x et y et la couleur de la fourmi si different de noir
     def __init__(self, grille, canvas, fx, fy, couleur_dessin='black'):
         self.canvas = canvas
         self.grille = grille
         self.pos_x = fx
         self.pos_y = fy
         self.orientation = HAUT
-        self.couleur_dessin = couleur_dessin # la couleur que la fourmi laissera derriere elle
+        self.couleur_dessin = couleur_dessin
 
-    # change la couleur sous la position actuelle de la fourmi
     def change_couleur(self):
-        if self.grille[self.pos_x][self.pos_y]==VIDE:
-           self.grille[self.pos_x][self.pos_y]=OCCUPE
-           # inverser la couleur sous la fourmi
-        canvas.dessine_case(self.pos_x,self.pos_y,self.couleur_dessin)
 
-        # choisir la couleur a dessiner
+        if grille[self.pos_x][self.pos_y]==OCCUPE:
+            grille[self.pos_x][self.pos_y]=VIDE
+            self.canvas.dessine_case(self.pos_x,self.pos_y,self.couleur_dessin)
 
-        # dessiner la nouvelle case sur le canvas
-        #self.grille[self.pos_x][self.pos_y].dessine_case()
+        elif grille[self.pos_x][self.pos_y]==VIDE:
+            grille[self.pos_x][self.pos_y]=OCCUPE
+            self.canvas.dessine_case(self.pos_x,self.pos_y,'white')
 
     def step(self):
-        if self.grille[self.pos_x][self.pos_y]==VIDE:
-           self.change_couleur()
-           #self.orientation=tourne_droite() corrige
 
-        # si la grille est vide on inverse la couleur et tourne a droite
-        else:
-             #canvas.change_couleur() corrige
-             self.orientation+=GAUCHE
-             #self.orientation=tourne_gauche() corrige
-        # si la grille est occupee on inverse la couleur et tourne a gauche
-        #self.dessine_case()
+        if grille[self.pos_x][self.pos_y]==VIDE:
+           self.change_couleur()
+           self.orientation=self.tourne_droite()
+
+        elif grille[self.pos_x][self.pos_y]==OCCUPE:
+            self.change_couleur()
+            self.orientation=self.tourne_gauche()
+
+        self.grille=self.avance()
+
 
     def tourne_gauche(self):
-        a=self.orientation % GAUCHE
-        self.orientation+=a
-        return (self.orientation)
-        # change l'orientation d'un cran vers la gauche
+        self.orientation+=1
+        self.orientation%= 4
+        return(self.orientation)
+
 
     def tourne_droite(self):
-        a=self.orientation % GAUCHE
-        self.orientation+=a
+        self.orientation-=1
+        self.orientation%= 4
         return(self.orientation)
-        # change l'orientation d'un cran vers la droite
+
+
 
     def avance(self):
-        self.grille[self.pos_x][self.pos_y].dessine_case()
-        # avance de une case en suivant la bonne orientation
+        if self.orientation == HAUT:
+            self.pos_y += 1
+        elif self.orientation == GAUCHE:
+            self.pos_x -= 1
+        elif self.orientation == DROITE:
+            self.pos_x += 1
+        elif self.orientation == BAS:
+            self.pos_y -= 1
 
 
 if __name__ == "__main__":
@@ -86,24 +93,15 @@ if __name__ == "__main__":
 
     canvas = CanvasFourmi(fenetre_principale)
 
-    # initialisation des fourmis
-    f1 = Fourmi(grille, canvas, 50, 50)
-    #f2 = Fourmi(grille, canvas, 60, 60, 'red')
-    #f3 = Fourmi(grille, canvas, 60, 50, 'green')
+    f1 = Fourmi(grille, canvas, 50, 50,'black')
+    f2 = Fourmi(grille, canvas, 60, 60, 'red')
+    f3 = Fourmi(grille, canvas, 60, 50, 'green')
 
-    # fonction appelee recursivement
     def fourmis():
         f1.step()
-        #f2.step()
-        #f3.step()
-
+        f2.step()
+        f3.step()
         fenetre_principale.after(10, fourmis)
 
-    #fenetre_principale.after(10, fourmis)
-
-    # a supprimer une fois que l'affichage marche
-    #canvas.dessine_case(0, 0, "black")
-    #canvas.dessine_case(50, 50, "red")
     fourmis()
-    print(grille)
     fenetre_principale.mainloop()
